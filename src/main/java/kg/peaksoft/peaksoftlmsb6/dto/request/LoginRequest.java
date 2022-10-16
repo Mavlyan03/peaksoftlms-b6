4e7d41960ac88c410d2bb294a13abb42a1732716
User <mavlyan.sadirov@gmail.com>
2022-10-16 20:34:08 +0600

    Sign in(jwt)

--- src/main/java/kg/peaksoft/peaksoftlmsb6/dto/request/LoginRequest.java
+++ src/main/java/kg/peaksoft/peaksoftlmsb6/dto/request/LoginRequest.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
+@NotBlank
 @Getter
 @Setter
 @AllArgsConstructor
--- src/main/java/kg/peaksoft/peaksoftlmsb6/service/UserService.java
+++ src/main/java/kg/peaksoft/peaksoftlmsb6/service/UserService.java
@@ -1,8 +1,5 @@
 package kg.peaksoft.peaksoftlmsb6.service;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import kg.peaksoft.peaksoftlmsb6.dto.request.LoginRequest;
 import kg.peaksoft.peaksoftlmsb6.dto.response.AuthResponse;
 import kg.peaksoft.peaksoftlmsb6.entity.User;
@@ -10,16 +7,12 @@ import kg.peaksoft.peaksoftlmsb6.exception.BadCredentialsException;
 import kg.peaksoft.peaksoftlmsb6.repository.UserRepository;
 import kg.peaksoft.peaksoftlmsb6.security.jwt.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
-import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -29,16 +22,6 @@ public class UserService  {
     private final AuthenticationManager authenticationManager;
     private final JwtTokenUtil jwtTokenUtil;
 
-
-    @PostConstruct
-    public void init() throws IOException {
-        GoogleCredentials googleCredentials =
-                GoogleCredentials.fromStream(new ClassPathResource("peaksoft-lms-6.json").getInputStream());
-
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(googleCredentials)
-                .build();
-    }
 
     public AuthResponse login(LoginRequest userRequest) {
         Authentication authentication = authenticationManager.authenticate(
