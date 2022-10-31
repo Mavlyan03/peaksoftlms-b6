de3a36dd16f43693a77c784f5df70d021aea0c95
User <mavlyan.sadirov@gmail.com>
2022-10-31 16:55:32 +0600

    LIFO for course and group

--- src/main/java/kg/peaksoft/peaksoftlmsb6/entity/Lesson.java
+++ src/main/java/kg/peaksoft/peaksoftlmsb6/entity/Lesson.java
@@ -29,7 +29,7 @@ public class Lesson {
     @OneToOne(cascade = ALL, mappedBy = "lesson")
     private Presentation presentation;
 
-    @OneToOne(cascade = ALL, mappedBy = "lesson")
+    @OneToOne(cascade = ALL, mappedBy = "lesson", orphanRemoval = true)
     private Task task;
 
     @OneToOne(cascade = ALL, mappedBy = "lesson")
