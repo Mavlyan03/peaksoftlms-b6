957dc88eb0614de835f67eec39d70d40e92bba83
Aiperi <adj155731@gmail.com>
2022-11-09 16:32:46 +0600

    Student excel

--- src/main/java/kg/peaksoft/peaksoftlmsb6/service/StudentService.java
+++ src/main/java/kg/peaksoft/peaksoftlmsb6/service/StudentService.java
@@ -1,12 +1,15 @@
 package kg.peaksoft.peaksoftlmsb6.service;
 
+import kg.peaksoft.peaksoftlmsb6.api.StudentApi;
 import kg.peaksoft.peaksoftlmsb6.dto.request.StudentRequest;
 import kg.peaksoft.peaksoftlmsb6.dto.response.SimpleResponse;
 import kg.peaksoft.peaksoftlmsb6.dto.response.StudentResponse;
 import kg.peaksoft.peaksoftlmsb6.entity.Group;
 import kg.peaksoft.peaksoftlmsb6.entity.Student;
 import kg.peaksoft.peaksoftlmsb6.entity.User;
+import kg.peaksoft.peaksoftlmsb6.entity.enums.Role;
 import kg.peaksoft.peaksoftlmsb6.entity.enums.StudyFormat;
+import kg.peaksoft.peaksoftlmsb6.exception.BadRequestException;
 import kg.peaksoft.peaksoftlmsb6.exception.NotFoundException;
 import kg.peaksoft.peaksoftlmsb6.repository.GroupRepository;
 import kg.peaksoft.peaksoftlmsb6.repository.StudentRepository;
@@ -84,33 +87,126 @@ public class StudentService {
         }
     }
 
+//    public List<StudentResponse> importExcel(MultipartFile file, Long id) throws IOException {
+//        List<Student> students = new ArrayList<>();
+//        Workbook workbook = WorkbookFactory.create(file.getInputStream(), "123456");
+//        for (Sheet sheet : workbook) {
+//            System.out.print(sheet.getSheetName());
+//        }
+//        Sheet sheet = workbook.getSheetAt(0);
+//        DataFormatter dataFormatter = new DataFormatter();
+//        for (Row row : sheet) {
+//            User user = new User();
+//            user.setEmail(row.getCell(6).getStringCellValue());
+//            for (Cell cell : row) {
+//                String cellValue = dataFormatter.formatCellValue(cell);
+//                System.out.print(cellValue);
+//            }
+//        }
+//        workbook.close();
+//        for (Student student : students) {
+//            Group groupEntity = groupRepository.findById(id).orElseThrow(
+//                    () -> new NotFoundException(String.format("Group with id =%s not found",id)));
+//            student.setGroup(groupEntity);
+//            studentRepository.save(student);
+//        }
+//        List<StudentResponse> studentResponses = new ArrayList<>();
+//        for (Student student: studentRepository.findAll()) {
+//            studentResponses.add(studentRepository.getStudent(student.getId()));
+//        }
+//        return studentResponses;
+//    }
+
     public List<StudentResponse> importExcel(MultipartFile file, Long id) throws IOException {
         String path = "C:\\Users\\user\\IdeaProjects\\peaksoftlms-b6\\students.xlsx";
-        FileInputStream inputStream = new FileInputStream(path);
-        Workbook workbook = WorkbookFactory.create(inputStream, "123456789");
+        List<Student> students = new ArrayList<>();
+        FileInputStream inputStream = new FileInputStream(String.valueOf(file));
+        Workbook workbook = WorkbookFactory.create(file.getInputStream());
+        System.out.println("Hello world!");
         for (Sheet sheet : workbook) {
             System.out.print(sheet.getSheetName());
         }
         Sheet sheet = workbook.getSheetAt(0);
         DataFormatter dataFormatter = new DataFormatter();
         for (Row row : sheet) {
-            User user = new User();
-            user.setEmail(row.getCell(6).getStringCellValue());
             for (Cell cell : row) {
                 String cellValue = dataFormatter.formatCellValue(cell);
                 System.out.print(cellValue);
+                Student student = new Student();
+                if(row.getCell(0)==null){
+                    throw new BadRequestException("FirstName in line index "+cell+" is empty!");
+                }else {
+                    student.setFirstName(row.getCell(0).getStringCellValue());
+                }
 
+                if(row.getCell(1)==null){
+                    throw new BadRequestException("LastName in line index "+cell+" is empty!");
+                }else {
+                    student.setLastName(row.getCell(1).getStringCellValue());
+                }
+
+                if(row.getCell(2)==null){
+                    throw new BadRequestException("Phone number in line index "+cell+" is empty!");
+                }else {
+                    student.setPhoneNumber(String.valueOf((long) row.getCell(2).getNumericCellValue()));
+                }
+
+                if(row.getCell(3, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL)==null){
+                    throw new BadRequestException("Study format in line index "+cell+" is empty!");
+                }else {
+                    student.setStudyFormat(StudyFormat.valueOf(row.getCell(3).getStringCellValue()));
+                }
+                User user = new User();
+
+                if(row.getCell(4,Row.MissingCellPolicy.RETURN_BLANK_AS_NULL)==null){
+                    throw new BadRequestException("Email in line index "+cell+" is empty!");
+                }else {
+                    user.setEmail(row.getCell(4).getStringCellValue());
+                }
+
+                if(row.getCell(5)==null){
+                    throw new BadRequestException("Role in line index "+cell+" is empty!");
+                }else {
+                    user.setRole(Role.valueOf(row.getCell(5).getStringCellValue()));
+                }
+
+                if(row.getCell(6)==null){
+                    throw new BadRequestException("Password in line index "+cell+" is empty!");
+                }else {
+                    user.setPassword(passwordEncoder.encode(String.valueOf(row.getCell(6).getStringCellValue())));
+                }
+
+                student.setUser(user);
+                students.add(student);
             }
         }
-        workbook.close();
-        inputStream.close();
+
+        for (Student student : students) {
+            Group groupEntity = groupRepository.findById(id).orElseThrow(
+                    () -> new NotFoundException(String.format("Group with id =%s not found",id)));
+            student.setGroup(groupEntity);
+            studentRepository.save(student);
+        }
+
         List<StudentResponse> studentResponses = new ArrayList<>();
-        for (Student student: studentRepository.findAll()) {
+        for (Student student : studentRepository.findAll()) {
             studentResponses.add(studentRepository.getStudent(student.getId()));
         }
         return studentResponses;
     }
+
+
 }
+//        workbook.close();
+//        inputStream.close();
+//        List<StudentResponse> studentResponses = new ArrayList<>();
+//        for (Student student : studentRepository.findAll()) {
+//            studentResponses.add(studentRepository.getStudent(student.getId()));
+//        }
+//        return studentResponses;
+
+
+
     //        PoijiOptions options = PoijiOptions.PoijiOptionsBuilder.settings()
 //                .password("123456789")
 //                .build();
