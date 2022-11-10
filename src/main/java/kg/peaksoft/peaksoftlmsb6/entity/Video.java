cb713f59e4462992590bfb76158afa97b06a6756
Abdumalik07 <koktash.sarkent@gmail.com>
2022-11-11 03:02:46 +0600

    Lesson crud

--- src/main/java/kg/peaksoft/peaksoftlmsb6/entity/Video.java
+++ src/main/java/kg/peaksoft/peaksoftlmsb6/entity/Video.java
@@ -3,6 +3,7 @@ package kg.peaksoft.peaksoftlmsb6.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
 
