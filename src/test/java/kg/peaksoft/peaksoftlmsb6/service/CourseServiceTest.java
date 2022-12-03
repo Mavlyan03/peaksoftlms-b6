1ba06295e4ca860825feb98ee813af6deeb64569
User <mavlyan.sadirov@gmail.com>
2022-12-03 22:26:48 +0600

    Junit

--- src/test/java/kg/peaksoft/peaksoftlmsb6/service/CourseServiceTest.java
+++ src/test/java/kg/peaksoft/peaksoftlmsb6/service/CourseServiceTest.java
@@ -1,17 +1,27 @@
 package kg.peaksoft.peaksoftlmsb6.service;
 
+import kg.peaksoft.peaksoftlmsb6.dto.request.AssignGroupRequest;
+import kg.peaksoft.peaksoftlmsb6.dto.request.AssignInstructorRequest;
 import kg.peaksoft.peaksoftlmsb6.dto.request.CourseRequest;
+import kg.peaksoft.peaksoftlmsb6.dto.request.DeleteInstructorFromCourseRequest;
+import kg.peaksoft.peaksoftlmsb6.dto.response.AssignInstructorResponse;
 import kg.peaksoft.peaksoftlmsb6.dto.response.CourseResponse;
+import kg.peaksoft.peaksoftlmsb6.dto.response.SimpleResponse;
+import kg.peaksoft.peaksoftlmsb6.dto.response.StudentResponse;
 import kg.peaksoft.peaksoftlmsb6.entity.Course;
 import kg.peaksoft.peaksoftlmsb6.exception.NotFoundException;
 import kg.peaksoft.peaksoftlmsb6.repository.CourseRepository;
+import kg.peaksoft.peaksoftlmsb6.repository.GroupRepository;
+import kg.peaksoft.peaksoftlmsb6.repository.InstructorRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 import java.time.LocalDate;
+import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -23,6 +33,11 @@ class CourseServiceTest {
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    private InstructorRepository instructorRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
     @Autowired
     private CourseService courseService;
 
@@ -100,5 +115,84 @@ class CourseServiceTest {
         request1.setImage("image");
 
         courseService.createCourse(request);
+    }
+
+    @Test
+    void getAllStudentsFromCourse() {
+        List<StudentResponse> students = courseService.getAllStudentsFromCourse(1L);
+
+        assertEquals(5, students.size());
+    }
+
+    @Test
+    void testGetAllInstructorsFromCourse() {
+        List<AssignInstructorResponse> instructors = courseService.getAllInstructorsFromCourse(1L);
+
+        assertEquals(1, instructors.size());
+    }
+
+    @Test
+    void testAssignInstructorToCourse() {
+        AssignInstructorRequest request = new AssignInstructorRequest();
+        request.setInstructorId(List.of(2L));
+        request.setCourseId(2L);
+
+        SimpleResponse result = courseService.assignInstructorToCourse(request);
+
+        Course course = courseRepository.findById(request.getCourseId()).orElseThrow(
+                () -> new NotFoundException("Course not found"));
+
+        assertNotNull(result);
+        assertThat(course.getInstructors().contains(instructorRepository.findById(2L)
+                .orElseThrow(() -> new NotFoundException("Instructor not found")))).isTrue();
+    }
+
+    @Test
+    void testAssignGroupToCourse() {
+        AssignGroupRequest request = new AssignGroupRequest();
+        request.setGroupId(2L);
+        request.setCourseId(2L);
+
+        SimpleResponse result = courseService.assignGroupToCourse(request);
+
+        Course course = courseRepository.findById(request.getCourseId()).orElseThrow(
+                () -> new NotFoundException("Course not found"));
+
+        assertNotNull(result);
+        assertThat(course.getGroup().contains(groupRepository.findById(2L)
+                .orElseThrow(() -> new NotFoundException("Group not found")))).isTrue();
+
+    }
+
+    @Test
+    void testUnassigned() {
+        DeleteInstructorFromCourseRequest request = new DeleteInstructorFromCourseRequest();
+        request.setInstructorId(2L);
+        request.setCourseId(2L);
+
+        SimpleResponse result = courseService.unassigned(request);
+
+        Course course = courseRepository.findById(request.getCourseId()).orElseThrow(
+                () -> new NotFoundException("Course not found"));
+
+        assertNotNull(result);
+        assertThat(course.getInstructors().contains(instructorRepository.findById(2L)
+                .orElseThrow(() -> new NotFoundException("Instructor not found")))).isFalse();
+    }
+
+    @Test
+    void testDeleteGroupFromCourse() {
+        AssignGroupRequest request = new AssignGroupRequest();
+        request.setGroupId(2L);
+        request.setCourseId(2L);
+
+        SimpleResponse result = courseService.deleteGroupFromCourse(request);
+
+        Course course = courseRepository.findById(request.getCourseId()).orElseThrow(
+                () -> new NotFoundException("Course not found"));
+
+        assertNotNull(result);
+        assertThat(course.getGroup().contains(groupRepository.findById(2L)
+                .orElseThrow(() -> new NotFoundException("Group not found")))).isFalse();
     }
 }
---
+++ src/test/java/kg/peaksoft/peaksoftlmsb6/service/TaskServiceTest.java
@@ -0,0 +1,29 @@
+package kg.peaksoft.peaksoftlmsb6.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskServiceTest {
+
+    @Test
+    void createTask() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void convertUpdateResponse() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void getTaskById() {
+    }
+
+}
