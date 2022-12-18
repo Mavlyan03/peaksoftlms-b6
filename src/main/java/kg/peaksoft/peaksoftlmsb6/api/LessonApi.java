545bd73e9f839d903c7e22842efb356eaa170a91
User <mavlyan.sadirov@gmail.com>
2022-12-18 11:06:20 +0600

    Test and result

--- src/main/java/kg/peaksoft/peaksoftlmsb6/api/LessonApi.java
+++ src/main/java/kg/peaksoft/peaksoftlmsb6/api/LessonApi.java
@@ -11,7 +11,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Deque;
 import java.util.List;
 
 @RestController
