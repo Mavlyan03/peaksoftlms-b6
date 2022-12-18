b4c175605fb67f3b07a68b6258e782be299eec26
User <mavlyan.sadirov@gmail.com>
2022-12-18 11:07:10 +0600

    Test and result

--- src/main/java/kg/peaksoft/peaksoftlmsb6/service/LessonService.java
+++ src/main/java/kg/peaksoft/peaksoftlmsb6/service/LessonService.java
@@ -11,9 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 
 @Service
